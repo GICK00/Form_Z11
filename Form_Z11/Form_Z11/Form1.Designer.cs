c0825ce82fcdfff0e4fed93c620c7a905574c64d
Gick <71695624+GICK00@users.noreply.github.com>
2021-12-14 17:13:40 +0300

    Add files via upload

---
+++ Form_Z11/Form_Z11/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿
+namespace Form_Z11
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        public void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxR = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxText = new System.Windows.Forms.TextBox();
+            this.buttonResult = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(63, 7);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(113, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Классы в C#";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 38);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Введите шаблон";
+            // 
+            // textBoxR
+            // 
+            this.textBoxR.Location = new System.Drawing.Point(108, 35);
+            this.textBoxR.Name = "textBoxR";
+            this.textBoxR.Size = new System.Drawing.Size(116, 20);
+            this.textBoxR.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 61);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(83, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Введите текст.";
+            // 
+            // textBoxText
+            // 
+            this.textBoxText.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxText.Location = new System.Drawing.Point(12, 80);
+            this.textBoxText.Multiline = true;
+            this.textBoxText.Name = "textBoxText";
+            this.textBoxText.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBoxText.Size = new System.Drawing.Size(212, 95);
+            this.textBoxText.TabIndex = 4;
+            // 
+            // buttonResult
+            // 
+            this.buttonResult.Location = new System.Drawing.Point(83, 181);
+            this.buttonResult.Name = "buttonResult";
+            this.buttonResult.Size = new System.Drawing.Size(75, 23);
+            this.buttonResult.TabIndex = 5;
+            this.buttonResult.Text = "Найти";
+            this.buttonResult.UseVisualStyleBackColor = true;
+            this.buttonResult.Click += new System.EventHandler(this.buttonResult_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 220);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(60, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Утин Д. С.";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(236, 242);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.buttonResult);
+            this.Controls.Add(this.textBoxText);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBoxR);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Form1";
+            this.Text = "Form Z11";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxR;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxText;
+        private System.Windows.Forms.Button buttonResult;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Form_Z11/Form_Z11/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace Form_Z11
+{
+    public partial class Form1 : Form
+    {
+        class Regular
+        {
+            private Regex r;
+            private string text;
+
+            public string Math()
+            {
+                bool match = r.IsMatch(text);
+                if (match == true)
+                    return "\r\nТекст содержит шаблон.\r\n";
+                else
+                    return "\r\nТекст не содержит шаблон.\r\n";
+            }
+            public string MathView()
+            {
+                string str = "";
+                MatchCollection match = r.Matches(text);
+                foreach (Match m in match)
+                    str += m + " ";
+                return str + "\r\n";
+            }
+            public string MatchDelet()
+            {
+                return r.Replace(text, "") + "\r\n";
+            }
+            public Regex R
+            {
+                get { return r; }
+                set { r = value; }
+            }
+            public string Text
+            {
+                get { return text; }
+                set { text = value; }
+            }
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void buttonResult_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Regular reg = new Regular();
+                reg.Text = Convert.ToString(textBoxText.Text);
+                if (reg.Text.Length <= 0)
+                    throw new Exception("Введите текст!");
+                reg.R = new Regex($@"{Convert.ToString(textBoxR.Text)}");
+                if (reg.R.ToString().Length <= 0)
+                    throw new Exception("Введите строку для поиска!");
+
+                
+                textBoxText.Text += reg.Math();
+                textBoxText.Text += reg.MathView();
+                textBoxText.Text += reg.MatchDelet();
+
+                textBoxText.Text += "Значения R - " + reg.R + ", Text - " + reg.Text;
+            }
+            catch (Exception ex)
+            {
+                textBoxText.Text = ex.Message;
+            }
+        }
+    }
+}
